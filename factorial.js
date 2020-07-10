5c1bf52c36e536be8b958f4ea5bcf1cf73637538
Alvin <fulmar38251@letterboxes.org>
2020-07-09 20:48:32 -0400

    update function factorial

--- factorial.js
+++ factorial.js
@@ -1,12 +1,17 @@
 /**
 Implement a recursive factorial() function which will take a number and compute its factorial value. For example, factorial(5) would return 120 because 5 * 4 * 3 * 2* 1 is 120.
  */
-function factorial (factorial) {
-    return '';
+function factorial (factorial2) {
+    if (factorial2 == 1 ) {
+        return 1
+    } else {
+        return factorial2 * factorial(factorial2 - 1);
+    }
 }
 
 const result1 = factorial(5)
 
+console.log(result1)
 /*
  Learn something new!
 
